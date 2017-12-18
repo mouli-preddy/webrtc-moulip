e73fa5fb4aa1eaa76db42ae84912bd55ff0114b9
Mouli Krishna <mouli.preddy@gmail.com>
2017-12-18 23:13:32 +0530

    setting up port

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var os = require('os');
 var nodeStatic = require('node-static');
 var http = require('http');
 var socketIO = require('socket.io');
-
+var port = process.env.PORT || 8010;
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(port);
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
